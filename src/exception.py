b815313460804882012d673a80977a7bf166b17c
yashhhp <yashpatilyp06@gmail.com>
2024-10-11 14:58:23 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -6,7 +6,7 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
